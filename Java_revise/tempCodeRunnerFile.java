5b3fe74e3a44b6d99aa508f9e1790f9a834b8e1c
Sameer shrinath <sameershrinath@gmail.com>
2026-03-17 17:06:45 +0530

    java added to git

---
+++ Java_revise/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+public class Star {
+    public static void main(String[] args){
+        int num=5;
+
+        for(int i=0 ; i<num; i++){
+            sSystem.out.println("*");
+        }
+    }
+}
