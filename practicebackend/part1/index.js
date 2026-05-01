11bf5d16bda5a6f0a70196cac362daa056574cbd
Sameer shrinath <sameershrinath@gmail.com>
2026-05-01 15:05:21 +0530

    simple get and dynamic get is implemented

--- practicebackend/part1/index.js
+++ practicebackend/part1/index.js
@@ -1,18 +1,40 @@
-const http = require("http");
 const express=require("express");
 
 const app=express();
-
+const usersdata=require("./MOCK_DATA.json");
 
 app.get("/",(req,res)=>{
     return res.send("hello from home page");
+});
+
+
+
+app.get("/users",(req,res)=>{
+    const html=`
+    <ul>
+    ${usersdata.map((user)=>`<li>${user.first_name} <br></li>`)}
+    </ul>
+    `;
+
+    res.send(html);
+});
+
+
+// app.get("/api/users",(req,res)=>{
+//     return res.json(usersdata);
+// })
+
+//Dynamic path parameters
+app.get("/api/users/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const userd=usersdata.find((user)=>user.id===id);
+    return res.json(userd);
+
 });
 
 app.get("/about",(req,res)=>{
     return res.send("hello from about page");
 });
 
-const myserver=http.createServer(app);
+app.listen(5000,()=>{console.log("server is running at port 5000....");})
 
-
-myserver.listen(5000,()=>{console.log("server started....")})
