f3a68e03d149dbea33c1dd324b46a0da8a9e4bce
Sameer shrinath <sameershrinath@gmail.com>
2026-05-01 16:35:06 +0530

    baisc crud done

--- practicebackend/part1/index.js
+++ practicebackend/part1/index.js
@@ -1,5 +1,5 @@
 const express=require("express");
-
+const fs=require("fs");
 const app=express();
 const usersdata=require("./MOCK_DATA.json");
 
@@ -8,6 +8,13 @@ app.get("/",(req,res)=>{
 });
 
 
+app.use(express.urlencoded({extended:false}));
+
+app.use((req,res,next)=>{
+    fs.appendFile("./log.txt",`${Date.now()}: ${req.method} ${req.url}`,(err,data)=>{
+        next();
+    });
+})
 
 app.get("/users",(req,res)=>{
     const html=`
@@ -20,17 +27,52 @@ app.get("/users",(req,res)=>{
 });
 
 
-// app.get("/api/users",(req,res)=>{
-//     return res.json(usersdata);
-// })
 
 //Dynamic path parameters
-app.get("/api/users/:id",(req,res)=>{
+app.route("/api/users/:id")
+.get((req,res)=>{
     const id=Number(req.params.id);
-    const userd=usersdata.find((user)=>user.id===id);
-    return res.json(userd);
+    const user=usersdata.find((user)=>user.id===id);
+    return res.json(user);
 
-});
+})
+.patch(
+    (req,res)=>{
+    return res.json({"status":"pending"});
+}
+)
+.delete(
+    (req,res)=>{
+    const id =Number(req.params.id);
+    const index=usersdata.findIndex(user=>user.id===id);
+    console.log(index);
+    if (index != -1 ){
+        usersdata.splice(index,1);
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(usersdata),(err,data)=>
+        {
+        return res.json({status:"successfully deleted",id_deleted:id});
+        }
+    );
+    }
+    else{
+        return res.json({status:"failed deleted"});
+    }
+    
+}
+);
+
+
+app.post("/api/users",(req,res)=>
+    {
+    const body=req.body;
+    usersdata.push({...body,id:usersdata.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(usersdata),(err,data)=>
+        {
+        return res.json({status:"successfully add",id:usersdata.length});
+        }
+    );
+    }
+);
 
 app.get("/about",(req,res)=>{
     return res.send("hello from about page");
